c172311f24f8c8e1d8eccc54ebdfbe4ddd09fa1f
estherdomfeh213 <domfehesther2020@gmail.com>
2024-09-12 22:12:36 +0000

    Add: Add a variable 'interest_earned' to script

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,6 @@
 principal = 1000
 rate= 0.05 
 time = 3
+interest_earned = principal * rate * time
 
-print("The simple interest is: " + str(principal * rate * time) )
+print("The simple interest is:", interest_earned )
