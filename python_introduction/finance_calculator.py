a64e7c6593c1e88a4928a3ba2a6219cdf6046842
estherdomfeh213 <domfehesther2020@gmail.com>
2024-09-13 01:26:31 +0000

    Modify data types

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,16 +1,16 @@
 # A  program for personal finance calculator 
 # user input for monthly income
-monthly_income = int(input("Enter your monthly income: "))
+monthly_income = input("Enter your monthly income: ")
 #user input for total monthly expenses
-monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_expenses =input("Enter your total monthly expenses: ")
 
 #calculate monthly savings 
-monthly_savings = (monthly_income - monthly_expenses)
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 
 #(Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05))
 #project annual saving using the formula above
 projectedSavings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print("Your monthly savings are $" + str(monthly_savings))
+print("Your monthly savings are $", (monthly_savings))
 
-print("Projected savings after one year, with interest, is: $" + str(projectedSavings))
+print("Projected savings after one year, with interest, is: $", projectedSavings)
