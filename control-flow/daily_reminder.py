4b52a168e2f0310fed18202b499eb4ccd83ab56f
estherdomfeh213 <domfehesther2020@gmail.com>
2024-09-21 11:32:38 +0000

    Mod: change match case statement

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,16 +1,16 @@
 while True:
   task = input("Enter your task: ")
-  is_priority = input("Priority (high/medium/low): ").lower()
+  priority = input("Priority (high/medium/low): ").lower()
   is_time = input("Is it time-bound? (yes/no): ").lower()
   
-  match is_priority:
+  match priority:
     case "high":
       if is_time == "yes":
-        print("Reminder:",task ,"is a", is_priority, "priority task that requires immediate attention today!")
+        print("Reminder:",task ,"is a", priority, "priority task that requires immediate attention today!")
     
     case "low":
       if is_time == "no":
-        print("Note:", task, "is a", is_priority, "priority task. Consider completing it when you have free time.")
+        print("Note:", task, "is a", priority, "priority task. Consider completing it when you have free time.")
         
     case _:
       print("Invalid input")
