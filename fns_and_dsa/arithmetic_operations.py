e20a00dc5b3de16217cabc75301649f6ac839cf9
estherdomfeh213 <domfehesther2020@gmail.com>
2024-09-26 23:00:40 +0000

    Add  a match case to handle division by zero error

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,9 +8,10 @@ def perform_operation(num1, num2, operation):
     case _ if operation == 'multiply':
       return num1 * num2
     case _ if operation == 'divide':
-      if num2 != 0:
-        return num1 / num2
-      else:
+      if num2 == 0:
         return "Division by zero error"
+      else:
+        return num1 / num2
+        
       
       
